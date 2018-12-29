64b0d851bf2f3a51db3e281ff37fc044fac9118b
Sergio Tabanelli <sergio.tabanelli@gmail.com>
2018-12-29 19:28:59 +0100

    feat(transformers): Added support for typescript tranformers injection in ng build process

--- packages/custom-webpack/src/custom-webpack-builder.spec.ts
+++ packages/custom-webpack/src/custom-webpack-builder.spec.ts
@@ -1,12 +1,33 @@
-import {MergeStrategies} from "./custom-webpack-builder-config";
+import { MergeStrategies } from "./custom-webpack-builder-config";
 jest.mock('./webpack-config-merger');
-import {CustomWebpackBuilder, defaultWebpackConfigPath} from "./custom-webpack-builder";
+jest.mock('@ngtools/webpack');
+import { CustomWebpackBuilder, defaultWebpackConfigPath } from "./custom-webpack-builder";
 import * as fs from 'fs';
-import {WebpackConfigMerger} from "./webpack-config-merger";
-import {Path} from '@angular-devkit/core';
+import { WebpackConfigMerger } from "./webpack-config-merger";
+import { Path } from '@angular-devkit/core';
+import { AngularCompilerPlugin } from '@ngtools/webpack';
+import * as ts from 'typescript';
+const transform0 = (context: ts.TransformationContext) => (rootNode: ts.SourceFile): ts.SourceFile => {
+	return rootNode;
+}
+
+const transform1 = (context: ts.TransformationContext) => (rootNode: ts.SourceFile): ts.SourceFile => {
+	return rootNode;
+}
+
+const transform2 = (context: ts.TransformationContext) => (rootNode: ts.SourceFile): ts.SourceFile => {
+	return rootNode;
+}
+
+const angularCompilerPluginMock: jest.Mocked<AngularCompilerPlugin> = new AngularCompilerPlugin([transform1] as any) as any;
 
 const baseWebpackConfig = {
 	entry: 'blah'
+};
+
+const baseWebpackConfigWithPlugin = {
+	entry: 'blah',
+	plugins: [angularCompilerPluginMock]
 };
 
 const customWebpackConfig = {
@@ -17,11 +38,26 @@ const customWebpackConfig = {
 				use: 'node-loader'
 			}
 		]
-	},
+	}
 };
 
-function createConfigFile(fileName: string){
-	jest.mock(`${__dirname}/${fileName}`, ()=>customWebpackConfig, {virtual: true});
+const ngtsWebpackConfig = {
+	module: {
+		rules: [
+			{
+				test: '.node',
+				use: 'node-loader'
+			}
+		]
+	},
+	ngts: {
+		before: [transform0],
+		after: [transform2]
+	}
+};
+
+function createConfigFile(fileName: string, config: Object = customWebpackConfig) {
+	jest.mock(`${__dirname}/${fileName}`, () => config, { virtual: true });
 }
 
 describe('CustomWebpackBuilder test', () => {
@@ -37,22 +73,34 @@ describe('CustomWebpackBuilder test', () => {
 	it('Should load the file specified in configuration', () => {
 		fileName = 'extra-webpack.config.js';
 		createConfigFile(fileName);
-		CustomWebpackBuilder.buildWebpackConfig(__dirname as Path, {path: 'extra-webpack.config.js'}, baseWebpackConfig);
+		CustomWebpackBuilder.buildWebpackConfig(__dirname as Path, { path: 'extra-webpack.config.js' }, baseWebpackConfig);
 		expect(WebpackConfigMerger.merge).toHaveBeenCalledWith(baseWebpackConfig, customWebpackConfig, undefined, undefined);
 	});
 
 	it('Should pass on merge strategies', () => {
 		fileName = defaultWebpackConfigPath;
 		createConfigFile(fileName);
-		const mergeStrategies: MergeStrategies = {'blah': 'prepend'};
-		CustomWebpackBuilder.buildWebpackConfig(__dirname as Path, {mergeStrategies}, baseWebpackConfig);
+		const mergeStrategies: MergeStrategies = { 'blah': 'prepend' };
+		CustomWebpackBuilder.buildWebpackConfig(__dirname as Path, { mergeStrategies }, baseWebpackConfig);
 		expect(WebpackConfigMerger.merge).toHaveBeenCalledWith(baseWebpackConfig, customWebpackConfig, mergeStrategies, undefined);
 	});
 
 	it('Should pass on replaceDuplicatePlugins flag', () => {
 		fileName = defaultWebpackConfigPath;
 		createConfigFile(fileName);
-		CustomWebpackBuilder.buildWebpackConfig(__dirname as Path, {replaceDuplicatePlugins: true}, baseWebpackConfig);
+		CustomWebpackBuilder.buildWebpackConfig(__dirname as Path, { replaceDuplicatePlugins: true }, baseWebpackConfig);
 		expect(WebpackConfigMerger.merge).toHaveBeenCalledWith(baseWebpackConfig, customWebpackConfig, undefined, true);
+	});
+
+	it('Should add tranformers to angular and delete ngts', () => {
+		const expected = [transform0, transform1, transform2];
+		fileName = 'ngts-extra-webpack.config.js';
+		createConfigFile(fileName, ngtsWebpackConfig);
+		CustomWebpackBuilder.buildWebpackConfig(__dirname as Path, { path: 'ngts-extra-webpack.config.js' }, baseWebpackConfigWithPlugin);
+		expect(angularCompilerPluginMock['_transformers']).toEqual(expected);
+	});
+
+	it('Should delete ngts', () => {
+		expect(ngtsWebpackConfig.ngts).toEqual(undefined);
 	});
 });
--- packages/custom-webpack/src/custom-webpack-builder.ts
+++ packages/custom-webpack/src/custom-webpack-builder.ts
@@ -1,14 +1,34 @@
-import {CustomWebpackBuilderConfig} from "./custom-webpack-builder-config";
-import {Configuration} from "webpack";
-import {getSystemPath, Path} from '@angular-devkit/core';
-import {WebpackConfigMerger} from "./webpack-config-merger";
+import { CustomWebpackBuilderConfig } from "./custom-webpack-builder-config";
+import { Configuration } from "webpack";
+import { getSystemPath, Path } from '@angular-devkit/core';
+import { WebpackConfigMerger } from "./webpack-config-merger";
+import { AngularCompilerPlugin } from "@ngtools/webpack"
+import * as ts from "typescript";
 
 export const defaultWebpackConfigPath = 'webpack.config.js';
 
 export class CustomWebpackBuilder {
-	static buildWebpackConfig(root: Path, config: CustomWebpackBuilderConfig, baseWebpackConfig: Configuration): Configuration{
+	static buildWebpackConfig(root: Path, config: CustomWebpackBuilderConfig, baseWebpackConfig: Configuration): Configuration {
+//		console.log("Building custom webpack config!!!");
 		const webpackConfigPath = config.path || defaultWebpackConfigPath;
 		const customWebpackConfig = require(`${getSystemPath(root)}/${webpackConfigPath}`);
+		let plugins = baseWebpackConfig.plugins;
+		if (plugins && customWebpackConfig.ngts) {
+			plugins.forEach((plugin, index, plugins) => {
+				if (plugin instanceof AngularCompilerPlugin) {
+					let tranformers = plugin['_transformers'] as ts.TransformerFactory<ts.SourceFile>[];
+					if (customWebpackConfig.ngts.after) {
+						let pt = customWebpackConfig.ngts.after as ts.TransformerFactory<ts.SourceFile>[];
+						tranformers.push(...pt);
+					}
+					if (customWebpackConfig.ngts.before) {
+						let ut = customWebpackConfig.ngts.before as ts.TransformerFactory<ts.SourceFile>[];
+						tranformers.unshift(...ut);
+					}
+				}
+			})
+			delete customWebpackConfig['ngts'];
+		}
 		return WebpackConfigMerger.merge(baseWebpackConfig, customWebpackConfig, config.mergeStrategies, config.replaceDuplicatePlugins);
 	}
 }
